cddbe9ace916158fcfd23be6b6a1d39f2b08a48f
Robert Buckley <rbuckley30@gmail.com>
2013-09-09 13:12:40 -0700

    finished simple sort in python

--- python/simple_sort/simple_sort.py
+++ python/simple_sort/simple_sort.py
@@ -1,13 +1,32 @@
 import sys
- 
+from decimal import *
+
 test_cases = open(sys.argv[1], 'r')
  
 for test in test_cases:
     # ignore test if it is an empty line
     # 'test' represents the test case, do something with it
     # ...
-    # ...
-
-    print test
+    # ignore test if it is an empty line
+    if not test.strip():
+        continue
     
+    test_line = test.strip("\n")
+    
+    string_list = test_line.split();
+    
+    try:
+        int_list = [Decimal(num_string) for num_string in string_list]
+    except:
+        continue
+        
+    int_list.sort()
+    
+    for sorted in int_list[:-1]:
+        #sys.stdout.write(sorted + " ")
+        sys.stdout.write(str(sorted) + " ")
+        
+    #sys.stdout.write(int_list[-1] + "\n")
+    sys.stdout.write(str(int_list[-1]) + "\n")
+        
 test_cases.close()
--- python/skeleton/skeleton.py
+++ python/skeleton/skeleton.py
@@ -1,43 +1,18 @@
-#include <stdio.h>
-#include <string.h>
-
-#define LINE_SIZE 1024
-int main (int argc, char **argv)
-{
-   FILE *f;
-   char line[LINE_SIZE], *p;
-
-   // Open file passed as argument
-   if (argc < 2 || !(f = fopen(argv[1], "r")))
-   {
-      fprintf(stderr, "Unable to open file argument\n");
-      return 1;
-   }
-
-   // Read lines from file
-   while (fgets(line, LINE_SIZE, f))
-   {
-      // You may want to remove the trailing '\n'
-      if ((p = strchr(line, '\n')))
-      {
-         *p = '\0';
-      }
-
-      // Skip empty lines
-      if (line[0] == '\0')
-      {
-         continue;
-      }
-
-      // Do something with the line
-      printf("%s\n", line);
-   }
-
-   // Paranoid check
-   if (ferror(f))
-   {
-      perror("I/O Error");
-   }
-
-   return 0;
-}
+import sys
+ 
+test_cases = open(sys.argv[1], 'r')
+ 
+for test in test_cases:
+    # 'test' represents the test case, do something with it
+    # ...
+    # ...
+    # ignore test if it is an empty line
+    if not test.strip():
+        continue
+    
+    # Remove the trailing newline
+    test_line = test.strip("\n")
+    
+    print test_line
+    
+test_cases.close()
